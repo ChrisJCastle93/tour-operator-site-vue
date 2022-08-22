79f2d6bcdf47098cfe4e524a53ee4aa0c3676336
Chris Castle <chrisjcastle93@gmail.com>
2022-08-22 19:22:57 +0200

    update API

--- client/src/components/ImageCarousel.vue
+++ client/src/components/ImageCarousel.vue
@@ -2,63 +2,64 @@
   <div id="carousel-container">
     <h1>Get beneath the surface of these destinations</h1>
     <div id="image-carousel">
-        <div v-for="(city, index) in cities" class="city-card" :key="index">
-        {{city.city}} {{city.url}}</div>
+      <div v-for="(city, index) in computedCities" class="city-card" :key="index">
+        {{ city.city }} {{ city.url }}
+        <!-- {{ city }} -->
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-
 import axios from 'axios';
 
 export default {
   props: ['tours'],
+  data() {
+    return {
+      cities: [],
+    };
+  },
 
   computed: {
-    cities() {
-      const cities = [];
+    computedCities() {
       this.tours.forEach((tour) => {
         const query = tour.city;
-        axios.get(
-          `https://api.unsplash.com/photos/random?client_id=xd8-hxjJkd6lN_mRyI12i38m5gozrXDZw4SaZVkmBes&query=${query}`,
-        ).then((response) => {
-          cities.push({ city: tour.city, img: response.data.urls.regular });
-          console.log(cities);
-        })
+        axios.get(`https://api.unsplash.com/photos/random?client_id=xd8-hxjJkd6lN_mRyI12i38m5gozrXDZw4SaZVkmBes&query=${query}`)
+          .then((response) => {
+            this.cities.push({ city: tour.city, img: response.data.urls.regular });
+          })
           .catch((err) => console.log(err));
       });
-      console.log(cities);
-      return cities;
+      return this.cities;
     },
   },
 };
+
 </script>
 
 <style scoped>
-
 #carousel-container {
-    width: 90%;
-    margin: auto;
+  width: 90%;
+  margin: auto;
 }
 
 h1 {
-    text-align: left;
+  text-align: left;
 }
 
 #image-carousel {
-    display: flex;
+  display: flex;
 }
 
 .city-card {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: red;
-    border-radius: 20px;
-    height: 300px;
-    width: 200px;
-    margin-right: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: red;
+  border-radius: 20px;
+  height: 300px;
+  width: 200px;
+  margin-right: 20px;
 }
-
 </style>
